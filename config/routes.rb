6e041c1ebd9b68778c219b295b52736b034575d7
Ltlyl-tk79 <taku.805740@gmail.com>
2021-06-09 07:06:54 +0000

    ユーザー認証機能/top,sign_up,loginページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+
+  resources :users, only: [:show, :index, :edit, :update]
+
+  resources :movies do
+    resources :movie_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
 end

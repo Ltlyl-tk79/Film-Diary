c51077e0cd367b8241bf0363fcdf228087ff31cf
Ltlyl-tk79 <taku.805740@gmail.com>
2021-06-12 10:26:41 +0000

    フォロー機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,20 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
 
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      get :following, :followers
+    end
+    
+    resource :relationships, only: [:create, :destroy]
+    
+  end
 
   resources :movies do
     resources :movie_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+
+  
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_073558) do
+ActiveRecord::Schema.define(version: 2021_06_12_070459) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_06_09_073558) do
     t.text "body"
     t.string "movie_image_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

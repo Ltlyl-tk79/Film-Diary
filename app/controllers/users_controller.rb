fbee2d0088be05b41420d8cb07e1018fb189609e
Ltlyl-tk79 <taku.805740@gmail.com>
2021-06-20 07:09:14 +0000

    sign_up機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :profile_imag)
+    params.require(:user).permit(:name, :profile_image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,16 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   attachment :profile_image, destroy: false
-  
-  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+
+  validates :name, presence: true
+  # length: {maximum: 20, minimum: 2}, uniqueness: true
 
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   # 自分がフォローする側の関係性
   has_many :followings, through: :relationships, source: :followed
   # 与フォロー関係を通じて参照→自分がフォローしている人
-  
+
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 自分がフォローされる側の関係性
   has_many :followers, through: :reverse_of_relationships, source: :follower
